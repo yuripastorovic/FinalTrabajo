4dbb283e91827a10e4fabb0d5a73db4ce0da2574
Jorge Pastor Rodriguez <116525569+yuripastorovic@users.noreply.github.com>
2023-05-13 02:41:57 +0200

    ESBOZO DE BASE DE DATOSsshhh

--- src/finaltrabajo/vistas/Princial.java
+++ src/finaltrabajo/vistas/Princial.java
@@ -32,7 +32,7 @@ public class Princial extends javax.swing.JFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGap(0, 647, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
